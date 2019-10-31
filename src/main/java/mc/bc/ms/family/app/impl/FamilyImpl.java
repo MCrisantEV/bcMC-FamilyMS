dd4a5a88112c2b3bf39d63d36c4fb6c92d9e7abf
MCrisantEV <mcrisant@everis.com>
2019-10-31 11:36:05 -0500

    API finds
    API finds - Names, me permite buscar por nombre y profesores
    
    Version v0.3

--- src/main/java/mc/bc/ms/family/app/impl/FamilyImpl.java
+++ src/main/java/mc/bc/ms/family/app/impl/FamilyImpl.java
@@ -153,48 +153,47 @@ public class FamilyImpl implements FamilyService{
 			return famRep.findById(gp.getId()).map(gf -> {
 				gp.setInstitute(gf.getInstitute());
 				gp.setRelationship(gf.getType());
-				
-				List<Person> listPerson = new ArrayList<>();
-				Flux.fromIterable(gf.getFamilyMembers()).map(lgf -> {
-					
-					return client.get().uri("/{id}", Collections.singletonMap("id", lgf.getId()))
-						.accept(APPLICATION_JSON_UTF8)
-						.retrieve()
-						.bodyToMono(Person.class).doOnNext(m -> {
-							Person p = new Person();
-							p.setId(m.getId());
-							p.setRelationship(m.getRelationship());
-							p.setNames(m.getNames());
-							listPerson.add(p);
-							System.out.println(m.getNames());
-						});
-				}).subscribe();
-				
-//				gf.getFamilyMembers().forEach(fr -> {
-//					Person p = new Person();
-//					p.setId(fr.getId());
-//					p.setRelationship(fr.getRelationship());
-//					
-//					client.get().uri("/{id}", Collections.singletonMap("id", fr.getId()))
-//					.accept(APPLICATION_JSON_UTF8)
-//					.retrieve()
-//					.bodyToMono(Person.class).doOnNext(glp -> {
-//						
-//						
-//						System.out.println(glp.getNames());
-//						
-////						return p;
-//					}).subscribe();
-//					listPerson.add(p);
-//					
-//				});
-				gp.setFamilyMembers(listPerson);
-//				gp.setFamilyMembers(gf.getFamilyMembers());
+				List<Person> list = new ArrayList<>();
+				gf.getFamilyMembers().forEach(fr -> {
+					Person p = new Person();
+					p.setId(fr.getId());
+					p.setRelationship(fr.getRelationship());
+					list.add(p);
+				});
+				gp.setFamilyMembers(list);
 				return gp;
 			});
-//			return null;
+		})
+		.filter(fil -> fil.getRelationship().equals(type))
+		.filter(fil -> fil.getInstitute().equals(institute))
+		.flatMap(m -> {
+			if(m.getFamilyMembers().isEmpty()) {
+				return Flux.just(m);
+			}else {
+				return Flux.fromIterable(m.getFamilyMembers()).flatMap(f -> {
+					
+					return client.get().uri("/{id}", Collections.singletonMap("id", f.getId()))
+					.accept(APPLICATION_JSON_UTF8)
+					.retrieve()
+					.bodyToMono(Person.class)
+					.flatMap(gp -> {
+						f.setNames(gp.getNames());
+						f.setLastNames(gp.getLastNames());
+						f.setGender(gp.getGender());
+						f.setDateBirth(gp.getDateBirth());
+						f.setTypeDoc(gp.getTypeDoc());
+						return Mono.just(f);
+					});
+				}).collectList().map(cl -> {
+					List<Person> lp = new ArrayList<>();
+					cl.forEach(fr -> {
+						lp.add(fr);
+					});
+					m.setFamilyMembers(lp);
+					return m;
+				});
+			}
 		});
-
 	}
 
 }
